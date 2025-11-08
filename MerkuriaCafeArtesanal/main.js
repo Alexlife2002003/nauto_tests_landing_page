8567d3fb9c53c1d5ee7025fae6b6eee1390096ae
Alexia Hernandez Martinez <89033131+Alexlife2002003@users.noreply.github.com>
2025-11-07 21:09:03 -0600

    chore: add MerkuriaCafeArtesanal/main.js

---
+++ MerkuriaCafeArtesanal/main.js
@@ -0,0 +1,23 @@
+function logCTA() {
+            console.log('Primary CTA clicked');
+        }
+        document.addEventListener('DOMContentLoaded', function() {
+            const ctas = document.querySelectorAll('.cta');
+            ctas.forEach(cta => cta.addEventListener('click', logCTA));
+        });
+
+(function(d,t){
+      var BASE_URL="https://app.chatwoot.com";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=BASE_URL+"/packs/js/sdk.js";
+      g.async=true;
+      s.parentNode.insertBefore(g,s);
+      g.onload=function(){
+        if (window.chatwootSDK) {
+          window.chatwootSDK.run({
+            websiteToken:"vdTKwXXuTyNw7QujBGyQ7K8n",
+            baseUrl:BASE_URL
+          });
+        }
+      };
+    })(document,"script");
