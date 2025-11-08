8d433b899f38c3554a182df411bb6ba90faeea37
Alexia Hernandez Martinez <89033131+Alexlife2002003@users.noreply.github.com>
2025-11-07 21:00:12 -0600

    chore: add MerkuriaMazapanes/main.js

---
+++ MerkuriaMazapanes/main.js
@@ -0,0 +1,30 @@
+function logCTAClick() {
+            console.log('CTA Clicked');
+        }
+        document.addEventListener('DOMContentLoaded', function() {
+            const anchors = document.querySelectorAll('a[href^="#"]');
+            anchors.forEach(anchor => {
+                anchor.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    document.querySelector(this.getAttribute('href')).scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                });
+            });
+        });
+
+(function(d,t){
+      var BASE_URL="https://app.chatwoot.com";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=BASE_URL+"/packs/js/sdk.js";
+      g.async=true;
+      s.parentNode.insertBefore(g,s);
+      g.onload=function(){
+        if (window.chatwootSDK) {
+          window.chatwootSDK.run({
+            websiteToken:"vdTKwXXuTyNw7QujBGyQ7K8n",
+            baseUrl:BASE_URL
+          });
+        }
+      };
+    })(document,"script");
