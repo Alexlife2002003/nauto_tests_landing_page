bbbcd1d96d552a50242a8a1a84da93e09b5fef83
Alexia Hernandez Martinez <89033131+Alexlife2002003@users.noreply.github.com>
2025-11-10 14:57:17 -0600

    chore: add another/main.js

--- another/main.js
+++ another/main.js
@@ -1,4 +1,4 @@
-document.querySelector('form').addEventListener('submit', function(e) {
+document.querySelector('form').addEventListener('submit', function (e) {
             e.preventDefault();
             console.log('Formulario enviado');
         });
