ad57fadb6efde240492fcedbe29dd8409a305abf
Alexia Hernandez Martinez <89033131+Alexlife2002003@users.noreply.github.com>
2025-11-10 10:06:06 -0600

    chore: add Escuela/main.js

--- Escuela/main.js
+++ Escuela/main.js
@@ -1,13 +1,7 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-            });
-        });
-
-        document.querySelector('form').addEventListener('submit', function(e) {
+document.getElementById('contact-form').addEventListener('submit', function(e) {
             e.preventDefault();
             console.log('Formulario enviado');
+            this.reset();
         });
 
 (function(d,t){
