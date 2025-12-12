356e6b46d8c9e9f89f44ac91032a1e8adbb74f8c
Alexia Hernandez Martinez <89033131+Alexlife2002003@users.noreply.github.com>
2025-12-11 18:25:21 -0600

    Update main.js

--- starter/main.js
+++ starter/main.js
@@ -1,15 +1,14 @@
-(function(d,t){
-      var BASE_URL="https://fulgid-unisomeric-marylee.ngrok-free.dev";
-      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-      g.src=BASE_URL+"/packs/js/sdk.js";
-      g.async=true;
-      s.parentNode.insertBefore(g,s);
-      g.onload=function(){
-        if (window.chatwootSDK) {
-          window.chatwootSDK.run({
-            websiteToken:"JwivGvHoav5RpDzZznbbQDnE",
-            baseUrl:BASE_URL
-          });
-        }
-      };
-    })(document,"script");
+  (function(d,t) {
+    var BASE_URL="https://fulgid-unisomeric-marylee.ngrok-free.dev:3000";
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=BASE_URL+"/packs/js/sdk.js";
+    g.async = true;
+    s.parentNode.insertBefore(g,s);
+    g.onload=function(){
+      window.chatwootSDK.run({
+        websiteToken: 'JwivGvHoav5RpDzZznbbQDnE',
+        baseUrl: BASE_URL
+      })
+    }
+  })(document,"script");
+
