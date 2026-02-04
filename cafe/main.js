310f974627cbd1f54ffb4bf5ee32c857b7d44103
Alexia Hernandez Martinez <89033131+Alexlife2002003@users.noreply.github.com>
2026-02-04 10:46:18 -0600

    Update main.js

--- cafe/main.js
+++ cafe/main.js
@@ -9,7 +9,7 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
         });
 
   (function(d,t) {
-    var BASE_URL="http://0.0.0.0:3000";
+    var BASE_URL="https://fulgid-unisomeric-marylee.ngrok-free.dev";
     var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=BASE_URL+"/packs/js/sdk.js";
     g.async = true;
